4f4fedf98723458c1a78015104b9e551884dde8a
Linda-Lydia <Lydia6125@163.com>
2020-09-06 11:20:58 +0800

    chart-done

--- app/src/main/java/com/java/raocongyuan/StatisticsFragment.java
+++ app/src/main/java/com/java/raocongyuan/StatisticsFragment.java
@@ -1,31 +1,46 @@
 package com.java.raocongyuan;
 
 import android.graphics.Color;
+import android.graphics.Matrix;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CompoundButton;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.Chart;
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
 import com.github.mikephil.charting.highlight.Highlight;
 import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.github.mikephil.charting.listener.ChartTouchListener;
+import com.github.mikephil.charting.listener.OnChartGestureListener;
 import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
 import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -116,10 +131,15 @@ public class StatisticsFragment extends Fragment {
         AccumulativeStatistics a2 = new AccumulativeStatistics(300,192,72);
         AccumulativeStatistics a3 = new AccumulativeStatistics(400,93,31);
         AccumulativeStatistics a4 = new AccumulativeStatistics(500,63,419);
+        bar_data.clear();
         bar_data.put("place1",a1);
-        bar_data.put("place2",a1);
-        bar_data.put("place3",a1);
-        bar_data.put("place4",a1);
+        bar_data.put("place2",a2);
+        bar_data.put("place3",a3);
+        bar_data.put("place4",a4);
+        line_data.clear();
+        line_data.put("CONFIRMED",new ArrayList<Integer>(Arrays.asList(62,137,200)));
+        line_data.put("CURED",new ArrayList<Integer>(Arrays.asList(47,56,132)));
+        line_data.put("DEAD",new ArrayList<Integer>(Arrays.asList(3,18,23)));
 
         toggleButton = view.findViewById(R.id.area_switch_button);
         line_chart = view.findViewById(R.id.line_chart_for_trend);
@@ -128,6 +148,10 @@ public class StatisticsFragment extends Fragment {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 toggleButton.setChecked(isChecked);
                 isInternational = isChecked;
+                //TODO::backend::get new status
+                bar_data.put("placeX",new AccumulativeStatistics(4872,1663,2419));
+                SetBarChart();
+                SetLineChart();
             }
 
         });
@@ -141,8 +165,7 @@ public class StatisticsFragment extends Fragment {
         //TODO::line_data, start_date, bar_data
 
         //initialize the properties for bar-chart
-        initChart(bar_chart);
-        initChart(line_chart);
+        initChart();
 
         //initialize the data
         SetBarChart();
@@ -151,41 +174,228 @@ public class StatisticsFragment extends Fragment {
         //bar_chart listener ---- for line chart
         bar_chart.setOnChartValueSelectedListener(new OnChartValueSelectedListener() {
             @Override
-            public void onValueSelected(Entry e, int dataSetIndex, Highlight h) {
-
+            public void onValueSelected(Entry e, Highlight h) {
+                int ic = 0;
+                for(LinkedHashMap.Entry<String,AccumulativeStatistics> entry : bar_data.entrySet()){
+                    Log.d("Entry e =",e.toString()+" -- e.getX() = "+e.getX());
+                    float ix = e.getX();
+                    if(Math.floor(ix)==ic){
+                        String place_name = entry.getKey();
+                        Log.d("place_name", place_name);
+                        //TODO::backend::get the trend-data for this place
+                        //line_data =
+                        //TODO::front::delete the fake statistics below
+                        line_data.clear();
+                        if(place_name=="place1"){
+                            line_data.put("CONFIRMED",new ArrayList<Integer>(Arrays.asList(62,137,200)));
+                            line_data.put("CURED",new ArrayList<Integer>(Arrays.asList(47,56,132)));
+                            line_data.put("DEAD",new ArrayList<Integer>(Arrays.asList(3,18,23)));
+                        }
+                        else if(place_name=="place2"){
+                            line_data.put("CONFIRMED",new ArrayList<Integer>(Arrays.asList(70,164,300)));
+                            line_data.put("CURED",new ArrayList<Integer>(Arrays.asList(13,96,192)));
+                            line_data.put("DEAD",new ArrayList<Integer>(Arrays.asList(16,58,72)));
+                        }
+                        SetLineChart();
+                        break;
+                    }
+                    else{
+                        ic++;
+                    }
+                }
             }
 
             @Override
             public void onNothingSelected() {
-                //TODO::front???
-                //???? do nothing ????
+                //TODO::front::???
+                //do nothing
+            }
+        });
+        bar_chart.setOnChartGestureListener(new OnChartGestureListener() {
+            @Override
+            public void onChartGestureStart(MotionEvent me, ChartTouchListener.ChartGesture lastPerformedGesture) {
+
+            }
+
+            @Override
+            public void onChartGestureEnd(MotionEvent me, ChartTouchListener.ChartGesture lastPerformedGesture) {
+
+            }
+
+            @Override
+            public void onChartLongPressed(MotionEvent me) {
+
+            }
+
+            @Override
+            public void onChartDoubleTapped(MotionEvent me) {
+
+            }
+
+            @Override
+            public void onChartSingleTapped(MotionEvent me) {
+
+            }
+
+            @Override
+            public void onChartFling(MotionEvent me1, MotionEvent me2, float velocityX, float velocityY) {
+
+            }
+
+            @Override
+            public void onChartScale(MotionEvent me, float scaleX, float scaleY) {
+
+            }
+
+            @Override
+            public void onChartTranslate(MotionEvent me, float dX, float dY) {
+
+            }
+        });
+        line_chart.setOnChartGestureListener(new OnChartGestureListener() {
+            @Override
+            public void onChartGestureStart(MotionEvent me, ChartTouchListener.ChartGesture lastPerformedGesture) {
+
+            }
+
+            @Override
+            public void onChartGestureEnd(MotionEvent me, ChartTouchListener.ChartGesture lastPerformedGesture) {
+
+            }
+
+            @Override
+            public void onChartLongPressed(MotionEvent me) {
+
+            }
+
+            @Override
+            public void onChartDoubleTapped(MotionEvent me) {
+
+            }
+
+            @Override
+            public void onChartSingleTapped(MotionEvent me) {
+
+            }
+
+            @Override
+            public void onChartFling(MotionEvent me1, MotionEvent me2, float velocityX, float velocityY) {
+
+            }
+
+            @Override
+            public void onChartScale(MotionEvent me, float scaleX, float scaleY) {
+
+            }
+
+            @Override
+            public void onChartTranslate(MotionEvent me, float dX, float dY) {
+
             }
         });
         bar_chart.setDrawGridBackground(false);//another listener???
     }
 
-    private void initChart(Chart chart){
+    private void initChart(){
         //TODO::front::is this chart the same instance of the original parameter?
-        chart.setBackgroundColor(Color.WHITE);//background color
-        chart.setTouchEnabled(true);//gesture enabled
+
+        //***chart settings for bar_chart***//
+        bar_chart.setBackgroundColor(Color.WHITE);//background color
+        bar_chart.setTouchEnabled(true);//gesture enabled
         bar_chart.setDragEnabled(true);//grad enabled //TODO::front::need to be false???
         bar_chart.setScaleEnabled(true);//scale enabled
         bar_chart.setScaleXEnabled(true);//scaleX enabled
         bar_chart.setScaleYEnabled(true);//scaleY enabled
         bar_chart.setPinchZoom(true);//zoom enabled
+
+        bar_chart.setDrawGridBackground(false);//not grid lines in the back
+        bar_chart.setDrawBarShadow(false);//no shadow
+        bar_chart.setHighlightFullBarEnabled(false);
+
+        //chart borders
+        bar_chart.setDrawBorders(true);
+        Matrix m = new Matrix();
+        m.postScale(0.9f, 1f);//两个参数分别是x,y轴的缩放比例。例如：将x轴的数据放大为之前的1.5倍
+        bar_chart.getViewPortHandler().refresh(m, bar_chart, false);//将图表动画显示之前进行缩放
+
+        //x-axis
+        XAxis bar_xAxis = bar_chart.getXAxis();
+        bar_xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        bar_xAxis.setAxisMinimum(0f);
+        bar_xAxis.setGranularity(1f);
+        bar_xAxis.setCenterAxisLabels(true);//label in center
+
+        //y-axis (double side)
+        YAxis bar_leftAxis = bar_chart.getAxisLeft();
+        YAxis bar_rightAxis = bar_chart.getAxisRight();
+        bar_leftAxis.setAxisMinimum(0f);
+        bar_rightAxis.setAxisMinimum(0f);
+
+        //legend settings (图例)
+        Legend bar_legend = bar_chart.getLegend();
+        bar_legend.setForm(Legend.LegendForm.LINE);
+        bar_legend.setTextSize(11f);
+        bar_legend.setVerticalAlignment(Legend.LegendVerticalAlignment.BOTTOM);//bottom of the chart
+        bar_legend.setHorizontalAlignment(Legend.LegendHorizontalAlignment.CENTER);//in the center of the chart
+        bar_legend.setOrientation(Legend.LegendOrientation.HORIZONTAL);//from left to right
+        bar_legend.setDrawInside(false);//draw outside
+
+
+        //***chart settings for line_chart***//
+        line_chart.setBackgroundColor(Color.WHITE);//background color
+        line_chart.setTouchEnabled(true);//gesture enabled
+        line_chart.setDragEnabled(true);//grad enabled //TODO::front::need to be false???
+        line_chart.setScaleEnabled(true);//scale enabled
+        line_chart.setScaleXEnabled(true);//scaleX enabled
+        line_chart.setScaleYEnabled(true);//scaleY enabled
+        line_chart.setPinchZoom(true);//zoom enabled
+
+        line_chart.setDrawGridBackground(false);//not grid lines in the back
+
+        //chart borders
+        line_chart.setDrawBorders(true);
+        Matrix line_m = new Matrix();
+        line_m.postScale(0.9f, 1f);//两个参数分别是x,y轴的缩放比例。例如：将x轴的数据放大为之前的1.5倍
+        line_chart.getViewPortHandler().refresh(line_m, line_chart, false);//将图表动画显示之前进行缩放
+
+        //x-axis
+        XAxis line_xAxis = line_chart.getXAxis();
+        line_xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        line_xAxis.setAxisMinimum(0f);
+        line_xAxis.setGranularity(1f);
+        line_xAxis.setCenterAxisLabels(false);//label in center
+
+        //y-axis (double side)
+        YAxis line_leftAxis = line_chart.getAxisLeft();
+        YAxis line_rightAxis = line_chart.getAxisRight();
+        line_leftAxis.setAxisMinimum(0f);
+        line_rightAxis.setAxisMinimum(0f);
+
+        //legend settings (图例)
+        Legend line_legend = line_chart.getLegend();
+        line_legend.setForm(Legend.LegendForm.LINE);
+        line_legend.setTextSize(11f);
+        line_legend.setVerticalAlignment(Legend.LegendVerticalAlignment.BOTTOM);//bottom of the chart
+        line_legend.setHorizontalAlignment(Legend.LegendHorizontalAlignment.CENTER);//in the center of the chart
+        line_legend.setOrientation(Legend.LegendOrientation.HORIZONTAL);//from left to right
+        line_legend.setDrawInside(false);//draw outside
+
     }
 
     private void SetBarChart(){
+        if(bar_data.size()==0)
+            return;
+
         List<BarEntry> confirmed_data = new ArrayList<BarEntry>();
         List<BarEntry> cured_data = new ArrayList<BarEntry>();
         List<BarEntry> dead_data = new ArrayList<BarEntry>();
-        List<String> place = new ArrayList<String>();
+        final List<String> place = new ArrayList<String>();
 
         int ic = 0;
         for(LinkedHashMap.Entry<String,AccumulativeStatistics> e : bar_data.entrySet()){
-            confirmed_data.add(new BarEntry(e.getValue().CONFIRMED,ic));
-            cured_data.add(new BarEntry(e.getValue().CURED,ic));
-            dead_data.add(new BarEntry(e.getValue().DEAD,ic));//val,xIndex
+            confirmed_data.add(new BarEntry(ic,e.getValue().CONFIRMED));
+            cured_data.add(new BarEntry(ic,e.getValue().CURED));
+            dead_data.add(new BarEntry(ic,e.getValue().DEAD));//val,xIndex
             place.add(e.getKey());
             ic++;
         }
@@ -193,45 +403,110 @@ public class StatisticsFragment extends Fragment {
         BarDataSet bardataset1 = new BarDataSet(confirmed_data, "CONFIRMED");
         bardataset1.setColor(Color.rgb(234,139,0));
         BarDataSet bardataset2 = new BarDataSet(cured_data, "CURED");
-        bardataset1.setColor(Color.rgb(48,190,48));
+        bardataset2.setColor(Color.rgb(48,190,48));
         BarDataSet bardataset3 = new BarDataSet(dead_data, "DEAD");
-        bardataset1.setColor(Color.rgb(200,0,0));
+        bardataset3.setColor(Color.rgb(200,0,0));
 
-        BarData barData = new BarData();
-        barData.addDataSet(bardataset1);
-        barData.addDataSet(bardataset2);
-        barData.addDataSet(bardataset3);
-        bar_chart.setData(barData);
-/*
+        //X轴自定义值
+        bar_chart.getXAxis().setValueFormatter(new IAxisValueFormatter() {
+            @Override
+            public String getFormattedValue(float value, AxisBase axis) {
+                if(value >= 0)
+                    return place.get((int) value % place.size());
+                else
+                    return "-1";
+            }
+        });
+        bar_chart.getXAxis().setAxisMaximum(place.size());
+
         List<IBarDataSet> dataSetList = new ArrayList<>();
         dataSetList.add(bardataset1);
         dataSetList.add(bardataset2);
         dataSetList.add(bardataset3);
-        bar_chart.setData(dataSetList);
-*/
-        bar_chart.animateY(5000);
+        BarData data = new BarData(dataSetList);
 
+        //需要显示柱状图的类别 数量
+        int barAmount = dataSetList.size();
+        //设置组间距占比30% 每条柱状图宽度占比 70% /barAmount  柱状图间距占比 0%
+        float groupSpace = 0.3f; //柱状图组之间的间距
+        float barWidth = (1f - groupSpace) / barAmount;
+        float barSpace = 0f;
+        //设置柱状图宽度
+        data.setBarWidth(barWidth);
+        //(起始点、柱状图组间距、柱状图之间间距)
+        data.groupBars(0f, groupSpace, barSpace);
+        bar_chart.notifyDataSetChanged();
+        bar_chart.invalidate();
+        bar_chart.animateY(5000);
+        bar_chart.setData(data);
     }
 
     private void SetLineChart(){
         //TODO::front::what if the Integer equals "null"
+        if(line_data.size()==0){
+            LineData data = new LineData();
+            line_chart.notifyDataSetChanged();
+            line_chart.invalidate();
+            line_chart.animateY(3000);
+            line_chart.setData(data);
+            return;
+        }
 
         List<Integer> confirmed_trend = line_data.get("CONFIRMED");
         List<Integer> cured_trend = line_data.get("CURED");
         List<Integer> dead_trend = line_data.get("DEAD");
 
+        if(confirmed_trend.size()==0){
+            LineData data = new LineData();
+            line_chart.notifyDataSetChanged();
+            line_chart.invalidate();
+            line_chart.animateY(3000);
+            line_chart.setData(data);
+            return;
+        }
+
         List<Entry> confirmed_data = new ArrayList<Entry>();
         List<Entry> cured_data = new ArrayList<Entry>();
         List<Entry> dead_data = new ArrayList<Entry>();
-        List<Integer> date_count = new ArrayList<Integer>();
+        final List<Integer> date_count = new ArrayList<Integer>();
 
-        int count = confirmed_data.size();
+        int count = confirmed_trend.size();
         for(int ic = 0; ic < count; ic++){
-            confirmed_data.add(new Entry(confirmed_trend.get(ic),ic));
-            cured_data.add(new Entry(cured_trend.get(ic),ic));
-            dead_data.add(new Entry(dead_trend.get(ic),ic));//val,xIndex
-            ic++;
-            date_count.add(ic);
+            System.out.println(ic+" "+confirmed_trend.get(ic)+" "+cured_trend.get(ic)+" "+dead_trend.get(ic));
+            confirmed_data.add(new Entry(ic,confirmed_trend.get(ic)));
+            cured_data.add(new Entry(ic,cured_trend.get(ic)));
+            dead_data.add(new Entry(ic,dead_trend.get(ic)));//val,xIndex
+            date_count.add(ic+1);
         }
+
+        LineDataSet linedataset1 = new LineDataSet(confirmed_data, "CONFIRMED");
+        linedataset1.setColor(Color.rgb(234,139,0));
+        LineDataSet linedataset2 = new LineDataSet(cured_data, "CURED");
+        linedataset2.setColor(Color.rgb(48,190,48));
+        LineDataSet linedataset3 = new LineDataSet(dead_data, "DEAD");
+        linedataset3.setColor(Color.rgb(200,0,0));
+
+        line_chart.getXAxis().setValueFormatter(new IAxisValueFormatter() {
+            @Override
+            public String getFormattedValue(float value, AxisBase axis) {
+                if(value >= 0 && value < date_count.size())
+                    return ("day "+date_count.get((int) value % date_count.size()));
+                else
+                    return "";
+            }
+        });
+        line_chart.getXAxis().setAxisMaximum(date_count.size());
+
+        List<ILineDataSet> dataSetList = new ArrayList<>();
+        dataSetList.add(linedataset1);
+        dataSetList.add(linedataset2);
+        dataSetList.add(linedataset3);
+        LineData data = new LineData(dataSetList);
+
+        line_chart.notifyDataSetChanged();
+        line_chart.invalidate();
+        line_chart.animateY(3000);
+        line_chart.setData(data);
+
     }
 }
